9a655852ad463a1107fe42e1f0c5d7bfdd624321
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-01-03 10:55:41 +0100

    tring another algrothem

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,39 +1,37 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
+"""
+Determines if all boxes can be unlocked.
+"""
 def canUnlockAll(boxes):
     """
-    Determines if all the boxes can be opened.
+    Determines if all boxes can be opened.
 
-    :param boxes: List of lists, where each inner list contains keys for other boxes.
-    :return: True if all boxes can be opened, else False.
+    Args:
+        boxes (list of lists): List of lists where each sublist contains keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
     """
-    n = len(boxes)  # Number of boxes
-    opened = [False] * n  # Keep track of opened boxes
+    n = len(boxes)
+    opened = [False] * n
     opened[0] = True  # The first box is unlocked
 
-    keys = [0]  # Start with the keys from the first box
+    keys = boxes[0]  # Start with the keys in the first box
 
-    while keys:
-        current_key = keys.pop()  # Take a key
+    for key in keys:
+        if 0 <= key < n and not opened[key]:
+            opened[key] = True
+            keys.extend(boxes[key])
 
-        # If the box corresponding to this key hasn't been opened yet
-        if not opened[current_key]:
-            opened[current_key] = True  # Mark the box as opened
+    return all(opened)
 
-            # Add all the keys inside this box to the keys list
-            for key in boxes[current_key]:
-                if 0 <= key < n and not opened[key]:
-                    keys.append(key)
-
-    # Check if all boxes have been opened
-    (opened)
-
-# Example usage:
 if __name__ == "__main__":
-   boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
+    # Example usage
+    boxes = [[1], [2], [3], []]
+    print(canUnlockAll(boxes))  # Output: True
 
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
+    boxes = [[1, 3], [3, 0, 1], [2], [0]]
+    print(canUnlockAll(boxes))  # Output: True
 
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
+    boxes = [[1, 2, 3], [], [4], [5], []]
+    print(canUnlockAll(boxes))  # Output: False
