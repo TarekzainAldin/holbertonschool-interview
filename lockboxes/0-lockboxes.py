24d30f9da2e351f697db1ead73481745f01868ad
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-01-24 10:20:36 +0100

    make the pp8

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""lockboxes"""
+"""Module to determine if all boxes can be unlocked."""
 
 
 def canUnlockAll(boxes):
     """
-      Determines if all the boxes can be opened.
-      Args:
-          boxes: List of list
-      Returns:
-            True or false
-      """
+    Determines if all the boxes can be opened.
+
+    Args:
+        boxes (list): A list of lists where each sublist contains keys.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
     n = len(boxes)
-    open = [False] * n
-    open[0] = True
-    keys = boxes[0]
+    opened = [False] * n  # Tracks the status of opened boxes
+    opened[0] = True  # The first box is always open
+    keys = boxes[0]  # Start with the keys from the first box
 
     while keys:
         key = keys.pop()
-        if key < n and not open[key]:
-            open[key] = True
-            keys.extend(boxes[key])
+        if 0 <= key < n and not opened[key]:
+            opened[key] = True
+            keys.extend(boxes[key])  # Add keys from the newly opened box
 
-    return all(open)
+    return all(opened)
