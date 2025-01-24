a7a58c447883b32f72b2fe55d4e25163c8fa32fc
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-01-24 10:25:53 +0100

    make the main .c and

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -5,39 +5,42 @@
  * insert_node - Inserts a number into a sorted singly linked list.
  * @head: Double pointer to the head of the list.
  * @number: The number to insert.
- * 
- * Return: The address of the new node, or NULL if it failed.
+ *
+ * Return: Address of the new node, or NULL if it failed.
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new_node, *current;
+    listint_t *new_node, *current, *prev;
+
+    if (!head)
+        return (NULL);
 
     /* Allocate memory for the new node */
     new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
+    if (!new_node)
         return (NULL);
-
     new_node->n = number;
     new_node->next = NULL;
 
-    /* Case 1: Insert at the beginning or into an empty list */
-    if (*head == NULL || (*head)->n >= number)
+
+    if (!(*head) || (*head)->n >= number)
     {
         new_node->next = *head;
         *head = new_node;
         return (new_node);
     }
 
-    /* Case 2: Traverse the list to find the insertion point */
+
     current = *head;
-    while (current->next != NULL && current->next->n < number)
+    while (current && current->n < number)
     {
+        prev = current;
         current = current->next;
     }
 
-    /* Insert the new node */
-    new_node->next = current->next;
-    current->next = new_node;
+
+    new_node->next = current;
+    prev->next = new_node;
 
     return (new_node);
 }
