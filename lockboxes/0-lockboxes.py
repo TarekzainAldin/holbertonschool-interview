c71da73068fe5465f1993cf51e3239ca836f83f6
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-01-24 10:11:55 +0100

    make the main in lecks box

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python3
-"""
-Determines if all boxes can be unlocked.
-"""
+"""lockboxes"""
+
+
 def canUnlockAll(boxes):
     """
-    Determines if all boxes can be opened.
-
-    Args:
-        boxes (list of lists): List of lists where each sublist contains keys to other boxes.
-
-    Returns:
-        bool: True if all boxes can be opened, False otherwise.
-    """
+      Determines if all the boxes can be opened.
+      Args:
+          boxes: List of list
+      Returns:
+            True or false
+      """
     n = len(boxes)
-    opened = [False] * n
-    opened[0] = True  # The first box is unlocked
+    open = [False] * n
+    open[0] = True
+    keys = boxes[0]
 
-    keys = boxes[0]  # Start with the keys in the first box
-
-    for key in keys:
-        if 0 <= key < n and not opened[key]:
-            opened[key] = True
+    while keys:
+        key = keys.pop()
+        if key < n and not open[key]:
+            open[key] = True
             keys.extend(boxes[key])
 
-    return all(opened)
-
-if __name__ == "__main__":
-    # Example usage
-    boxes = [[1], [2], [3], []]
-    print(canUnlockAll(boxes))  # Output: True
-
-    boxes = [[1, 3], [3, 0, 1], [2], [0]]
-    print(canUnlockAll(boxes))  # Output: True
-
-    boxes = [[1, 2, 3], [], [4], [5], []]
-    print(canUnlockAll(boxes))  # Output: False
+    return all(open)
