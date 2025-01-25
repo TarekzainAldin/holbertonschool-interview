9306b9aae934868b902a5b3a967e0a3ddb921559
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-01-25 12:06:37 +0100

    finsh from betty

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -8,39 +8,37 @@
  *
  * Return: Address of the new node, or NULL if it failed.
  */
+
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new_node, *current, *prev;
+	listint_t *new_node, *current, *prev;
 
-    if (!head)
-        return (NULL);
+	if (!head)
+		return (NULL);
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(listint_t));
-    if (!new_node)
-        return (NULL);
-    new_node->n = number;
-    new_node->next = NULL;
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = number;
+	new_node->next = NULL;
 
+	if (!(*head) || (*head)->n >= number)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-    if (!(*head) || (*head)->n >= number)
-    {
-        new_node->next = *head;
-        *head = new_node;
-        return (new_node);
-    }
+	current = *head;
+	while (current && current->n < number)
+	{
+		prev = current;
+		current = current->next;
+	}
 
+	new_node->next = current;
+	prev->next = new_node;
 
-    current = *head;
-    while (current && current->n < number)
-    {
-        prev = current;
-        current = current->next;
-    }
-
-
-    new_node->next = current;
-    prev->next = new_node;
-
-    return (new_node);
+	return (new_node);
 }
