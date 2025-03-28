772246ec4104ac0e235232e9ed7b78711327d8a9
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-03-28 14:55:30 +0100

    make pycodestle for the file

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 import sys
 
+
 def is_safe(board, row, col, n):
     """Check if a queen can be placed at board[row][col]"""
     # Check this row on left side
     for i in range(col):
         if board[row][i] == 1:
             return False
-    
+
     # Check upper diagonal on left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-    
+
     # Check lower diagonal on left side
     for i, j in zip(range(row, n, 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-    
+
     return True
+
 
 def solve_nqueens_util(board, col, n, solutions):
     """Use backtracking to find all solutions"""
@@ -31,12 +33,13 @@ def solve_nqueens_util(board, col, n, solutions):
                     solution.append([i, j])
         solutions.append(solution)
         return
-    
+
     for i in range(n):
         if is_safe(board, i, col, n):
             board[i][col] = 1
             solve_nqueens_util(board, col + 1, n, solutions)
             board[i][col] = 0  # backtrack
+
 
 def solve_nqueens(n):
     """Solve the N Queens problem and print all solutions"""
@@ -45,25 +48,27 @@ def solve_nqueens(n):
     solve_nqueens_util(board, 0, n, solutions)
     return solutions
 
+
 def main():
     """Handle command line arguments and solve N Queens"""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
     try:
         n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    
+
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
+
     solutions = solve_nqueens(n)
     for solution in solutions:
         print(solution)
+
 
 if __name__ == "__main__":
     main()
